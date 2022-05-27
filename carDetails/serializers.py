5a45f8b06288dfd89074625da1db24ebb5c0bd08
Abhinav Jha <abhinavjha98ald@gmail.com>
2022-05-27 13:04:47 +0530

    Minor chanegs

--- carDetails/serializers.py
+++ carDetails/serializers.py
@@ -19,15 +19,17 @@ class PCNTableSerializer(serializers.ModelSerializer):
         try:
             cardetails = CarDetails.objects.filter(id=obj.id)
             print(cardetails)
-            data = [{"number_plate": i.number_plate,"car_model":i.car_model,"car_color":i.car_color,"car_location":i.car_location,"car_image":self.context['request'].build_absolute_uri(i.car_image.url)} for i in cardetails]
+            data = [{"number_plate": i.number_plate,"car_model":i.car_model,"car_color":i.car_color,"car_location":i.car_location,"car_image":self.context['request'].build_absolute_uri(i.car_image.url)if i.car_image else None} for i in cardetails]
             return data
         except:
             return None
 
     def get_pcnCode(self,obj):
         try:
-            pcnCode = PCNCode.objects.filter(id=obj.id)
-            print(pcnCode)
+            print(obj.id)
+            pcnTable = PCNTable.objects.get(id=obj.id)
+            print(pcnTable.pcnCode.slug)
+            pcnCode = PCNCode.objects.filter(slug=pcnTable.pcnCode.slug)
             data = [{"pcnCode": i.pcnCode,"slug":i.slug} for i in pcnCode]
             return data
         except:
